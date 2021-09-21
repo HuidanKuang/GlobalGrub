63e439139785b2bccd5936416aa22522a397d469
HuidanKuang <77803403+HuidanKuang@users.noreply.github.com>
2021-09-21 11:03:58 -0400

    lesson2

--- GlobalGrub/Controllers/CategoriesController.cs
+++ GlobalGrub/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GlobalGrub.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,30 @@ namespace GlobalGrub.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // use the Category model to create 10 categories objects in memory
+            var categories = new List<Category>();
+
+            for (var i = 1; i < 11; i++)
+            {
+                categories.Add(new Category() { CategoryId = i, Name = "Category " + i.ToString() });
+            }
+
+            // pass the categories list as strongly-typed data to the view for display
+            return View(categories);
         }
 
-        //Create /categories/browse?category=abc - show selected category
+        // /categories/browse?category=abc - show selected category 
         public IActionResult Browse(string category)
         {
-            //store the input parameter in a var inside the ViewBag so we can display the user's selection
+            // ensure we have a category value
+            if (category == null)
+            {
+                // redirect the user to Index so they can choose a category
+                return RedirectToAction("Index");
+            }
+
+
+            // store the input parameter in a var inside the ViewBag so we can display the user's selection
             ViewBag.category = category;
 
             return View();
