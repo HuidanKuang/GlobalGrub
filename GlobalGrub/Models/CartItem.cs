1b4a91216e04ed2b09d8c6537423595c34ff5423
HuidanKuang <77803403+HuidanKuang@users.noreply.github.com>
2021-09-28 12:37:46 -0400

    l4 p1 model creation

---
+++ GlobalGrub/Models/CartItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalGrub.Models
+{
+    public class CartItem
+    {
+        public int CartItemId { get; set; }
+        [Range(1, 999999)]
+        public int Quantity { get; set; }
+
+        [Range(0.01, 999999)]
+        public double Price { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string UserId { get; set; }
+
+        public Product Product { get; set; }//represent the parent object(1 product to many cartitems)
+
+    }
+}
--- GlobalGrub/Models/Category.cs
+++ GlobalGrub/Models/Category.cs
@@ -8,14 +8,23 @@ namespace GlobalGrub.Models
 {
     public class Category
     {
-        //all pk fields in asp.net mvc should be called either {Model}Id or Id
+        //all pk(primary key) fields in asp.net mvc should be called either {Model}Id or Id
         //property names should always use PascalCase
+
+
 
         [Range(0,999999)]
         [Display(Name = "Category Id")] //this set an alias for all labels globally
+
+        //1st step: add a primary key of the Category model
         public int CategoryId { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "And no empty strings!")]
+
+        //2nd step: add a "name" property
         public string Name { get; set; }
+
+        //child ref
+        public List<Product> Products { get; set; }
     }
 }
